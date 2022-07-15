a51909cbd09bdb5872e3339f7cb52f05216c7142
Flexible.ChanwooKIm <flexible.chan@gmail.com>
2022-07-15 18:12:05 +0900

    물방물 및 물결 애니메이션 추가

---
+++ src/app/example/gsap/ex08/ex08.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ex08',
+  templateUrl: './ex08.component.html',
+  styleUrls: ['./ex08.component.scss']
+})
+export class Ex08Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/example/gsap/gsap.module.ts
+++ src/app/example/gsap/gsap.module.ts
@@ -8,6 +8,7 @@ import {Ex04Component} from './ex04/ex04.component';
 import { Ex05Component } from './ex05/ex05.component';
 import { Ex06Component } from './ex06/ex06.component';
 import { Ex07Component } from './ex07/ex07.component';
+import { Ex08Component } from './ex08/ex08.component';
 
 const routes: Routes = [
   {path: 'ex01', component: Ex01Component},
@@ -17,6 +18,7 @@ const routes: Routes = [
   {path: 'ex05', component: Ex05Component},
   {path: 'ex06', component: Ex06Component},
   {path: 'ex07', component: Ex07Component},
+  {path: 'ex08', component: Ex08Component},
   {path: '', redirectTo: 'ex01', pathMatch: 'full'},
 ];
 
@@ -28,7 +30,8 @@ const routes: Routes = [
     Ex04Component,
     Ex05Component,
     Ex06Component,
-    Ex07Component
+    Ex07Component,
+    Ex08Component
   ],
   imports: [CommonModule, RouterModule.forChild([...routes]),]
 })
