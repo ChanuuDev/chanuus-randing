6d4ec423fbc928932c64d458e56dfabb8e327558
Flexible.ChanwooKIm <flexible.chan@gmail.com>
2022-11-04 14:42:44 +0900

    added randing sample

---
+++ src/app/randing/randing-temporarily/randing-temporarily.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-randing-temporarily',
+  templateUrl: './randing-temporarily.component.html',
+  styleUrls: ['./randing-temporarily.component.scss']
+})
+export class RandingTemporarilyComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  clickEl(event: any): void {
+    console.log('click event ', event);
+  }
+
+}
--- src/app/randing/randing.module.ts
+++ src/app/randing/randing.module.ts
@@ -1,21 +1,17 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RandingComponent} from './randing.component';
-import {RouterModule, Routes} from "@angular/router";
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RandingComponent } from './randing.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RandingTemporarilyComponent } from './randing-temporarily/randing-temporarily.component';
 
 const routes: Routes = [
-  {path: 'randing', component: RandingComponent },
-  {path: '', redirectTo: 'randing', pathMatch: 'full'},
+  { path: 'randing', component: RandingComponent },
+  { path: 'randing-temporarily', component: RandingTemporarilyComponent },
+  { path: '', redirectTo: 'randing', pathMatch: 'full' },
 ];
 
 @NgModule({
-  declarations: [
-    RandingComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([...routes]),
-  ]
+  declarations: [RandingComponent, RandingTemporarilyComponent],
+  imports: [CommonModule, RouterModule.forChild([...routes])],
 })
-export class RandingModule { }
+export class RandingModule {}
